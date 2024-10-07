000f1595d95bbd227731952ba923eeaaab8d5720
Quinn Karpowitz <qkarpowi@byu.edu>
2024-10-07 14:23:43 -0600

    More set up

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -1,8 +1,11 @@
 import chess.*;
-
+import server.*;
 public class Main {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Server: " + piece);
+        System.out.println("♕ 240 Chess server.Server: " + piece);
+
+        var server = new Server();
+        server.run(8080);
     }
 }
