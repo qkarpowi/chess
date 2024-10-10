7d27f001df2ba9c2a75bd27e6d18c3dbb2764d10
Quinn Karpowitz <qkarpowi@byu.edu>
2024-10-10 08:59:57 -0600

    Further GameDAO changes

--- server/src/main/java/dataaccess/MemoryGameDAO.java
+++ server/src/main/java/dataaccess/MemoryGameDAO.java
@@ -10,22 +10,18 @@ public class MemoryGameDAO implements GameDAO {
     final private HashMap<Integer, GameData> games = new HashMap<>();
     private Integer nextId = 1;
 
-    @Override
     public void clear(){
         games.clear();
     }
 
-    @Override
     public Collection<GameData> getAllGames(){
         return games.values();
     }
 
-    @Override
     public GameData getGame(int id){
         return games.get(id);
     }
 
-    @Override
     public GameData createGame(String whiteUsername, String blackUsername, String gameName, ChessGame game){
         var gameData = new GameData(nextId++, whiteUsername, blackUsername, gameName, game);
         games.put(gameData.gameID(), gameData);
