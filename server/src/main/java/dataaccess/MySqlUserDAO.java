00585164a3f0ef1dd8532fe8ce3f7e4deac82f4c
Quinn Karpowitz <qkarpowi@byu.edu>
2024-10-09 18:29:32 -0600

    Completed user dao

---
+++ server/src/main/java/dataaccess/MySqlUserDAO.java
@@ -0,0 +1,81 @@
+package dataaccess;
+
+import exception.ResponseException;
+import model.UserData;
+
+import java.sql.*;
+
+
+public class MySqlUserDAO implements UserDAO {
+    public MySqlUserDAO() throws ResponseException, DataAccessException {
+        configureDatabase();
+    }
+
+    public void clear() throws DataAccessException {
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var preparedStatement = conn.prepareStatement("DELETE FROM User;")){
+                preparedStatement.executeUpdate();
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException("unable to drop table");
+        }
+    }
+
+    public UserData createUser(UserData user) throws DataAccessException {
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var preparedStatement = conn.prepareStatement("INSERT INTO User (Username, Password, Email) VALUES(?, ?, ?)")){
+                preparedStatement.setString(1, user.username());
+                preparedStatement.setString(2, user.password());
+                preparedStatement.setString(3, user.email());
+
+                preparedStatement.executeUpdate();
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException("unable to insert values");
+        }
+        return user;
+    }
+
+    public UserData getUser(String username) throws DataAccessException {
+        try (var conn = DatabaseManager.getConnection()) {
+            var statement = "SELECT Username, Password, Email FROM User WHERE Username=?";
+            try (var ps = conn.prepareStatement(statement)) {
+                ps.setString(1, username);
+                try (var rs = ps.executeQuery()) {
+                    if (rs.next()) {
+                        return new UserData(rs.getString("Username"), rs.getString("Password"), rs.getString("Email"));
+                    }
+                }
+            }
+        } catch (Exception e) {
+            throw new DataAccessException(String.format("Unable to read data: %s", e.getMessage()));
+        }
+        return null;
+    }
+
+
+
+    private final String[] createStatements = {
+            """
+            CREATE TABLE `User` (
+              `Username` varchar(100) NOT NULL,
+              `Password` varchar(100) NOT NULL,
+              `Email` varchar(100) NOT NULL,
+              PRIMARY KEY (`Username`)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+            """
+    };
+
+    private void configureDatabase() throws ResponseException, DataAccessException {
+        DatabaseManager.createDatabase();
+        try (var conn = DatabaseManager.getConnection()) {
+            for (var statement : createStatements) {
+                try (var preparedStatement = conn.prepareStatement(statement)) {
+                    preparedStatement.executeUpdate();
+                }
+            }
+        } catch (SQLException | DataAccessException ex) {
+            throw new ResponseException(500, String.format("Unable to configure database: %s", ex.getMessage()));
+        }
+    }
+}
