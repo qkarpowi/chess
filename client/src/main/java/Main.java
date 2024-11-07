b6a2598e94a344d90979638cade6c8054810526f
Quinn Karpowitz <qkarpowi@byu.edu>
2024-11-07 12:48:45 -0700

    Implmented the start of ServerFacade and added first three methods

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -2,7 +2,7 @@ import chess.*;
 
 public class Main {
     public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ Welcome to Chess. Sign in to start. ♕" + piece);
+        //var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
+        System.out.println("♕ Welcome to Chess. Sign in to start. ♕");
     }
 }
---
+++ client/src/main/java/ServerFacade.java
@@ -0,0 +1,91 @@
+import com.google.gson.Gson;
+import exception.ResponseException;
+
+import model.*;
+
+import java.io.*;
+import java.net.*;
+
+public class ServerFacade {
+    private final String serverUrl;
+
+    private String authToken = null;
+
+    public ServerFacade(String url) {
+        serverUrl = url;
+    }
+
+    public AuthData Register(UserData user) throws ResponseException {
+        var path = "/user";
+        var result = this.makeRequest("POST", path, user, AuthData.class);
+        this.authToken = result.authToken();
+        return result;
+    }
+
+    public AuthData login(LoginData login) throws ResponseException{
+        var path = "/session";
+        var result = this.makeRequest("POST", path, login, AuthData.class);
+        this.authToken = result.authToken();
+        return result;
+    }
+
+    public void logout() throws ResponseException {
+        var path = "/session";
+        this.makeRequest("DELETE", path, null, null);
+        authToken = null;
+    }
+
+    private <T> T makeRequest(String method, String path, Object request, Class<T> responseClass) throws ResponseException {
+        try {
+            URL url = (new URI(serverUrl + path)).toURL();
+            HttpURLConnection http = (HttpURLConnection) url.openConnection();
+            http.setRequestMethod(method);
+            http.setDoOutput(true);
+            if(this.authToken != null) {
+                http.setRequestProperty("Authorization", this.authToken);
+            }
+
+            writeBody(request, http);
+            http.connect();
+            throwIfNotSuccessful(http);
+            return readBody(http, responseClass);
+        } catch (Exception ex) {
+            throw new ResponseException(500, ex.getMessage());
+        }
+    }
+
+
+    private static void writeBody(Object request, HttpURLConnection http) throws IOException {
+        if (request != null) {
+            http.addRequestProperty("Content-Type", "application/json");
+            String reqData = new Gson().toJson(request);
+            try (OutputStream reqBody = http.getOutputStream()) {
+                reqBody.write(reqData.getBytes());
+            }
+        }
+    }
+
+    private void throwIfNotSuccessful(HttpURLConnection http) throws IOException, ResponseException {
+        var status = http.getResponseCode();
+        if (!isSuccessful(status)) {
+            throw new ResponseException(status, "failure: " + status);
+        }
+    }
+
+    private static <T> T readBody(HttpURLConnection http, Class<T> responseClass) throws IOException {
+        T response = null;
+        if (http.getContentLength() < 0) {
+            try (InputStream respBody = http.getInputStream()) {
+                InputStreamReader reader = new InputStreamReader(respBody);
+                if (responseClass != null) {
+                    response = new Gson().fromJson(reader, responseClass);
+                }
+            }
+        }
+        return response;
+    }
+
+    private boolean isSuccessful(int status) {
+        return status / 100 == 2;
+    }
+}
