6d644c71e2598c256ea47b51c036131c58881fb5
Quinn Karpowitz <qkarpowi@byu.edu>
2024-11-13 14:40:33 -0700

    Added bare bones to repl and login client

---
+++ client/src/main/java/ui/LoginClient.java
@@ -0,0 +1,47 @@
+package ui;
+
+import server.ServerFacade;
+
+import java.util.Arrays;
+
+public class LoginClient {
+    private final ServerFacade server;
+    private final String serverUrl;
+
+    public LoginClient(String serverUrl) {
+        server = new ServerFacade(serverUrl);
+        this.serverUrl = serverUrl;
+    }
+
+    public String eval(String input) {
+        try {
+            var tokens = input.toLowerCase().split(" ");
+            var cmd = (tokens.length > 0) ? tokens[0] : "help";
+            var params = Arrays.copyOfRange(tokens, 1, tokens.length);
+            return switch (cmd) {
+                case "login" -> login(params);
+                case "register" -> register(params);
+                case "quit" -> "quit";
+                default -> help();
+            };
+        } catch (Exception ex) {
+            return ex.getMessage();
+        }
+    }
+
+    private String login(String... params){
+        return "";
+    }
+
+    private String register(String... params) {
+        return "";
+    }
+
+    public String help() {
+            return """
+                    - signIn <yourname>
+                    - quit
+                    """;
+
+    }
+}
--- client/src/main/java/ui/Repl.java
+++ client/src/main/java/ui/Repl.java
@@ -1,4 +1,39 @@
 package ui;
 
+import java.util.Scanner;
+
+import static ui.EscapeSequences.*;
+
 public class Repl {
+    private final LoginClient client;
+
+    public Repl(String serverUrl) {
+        client = new LoginClient(serverUrl);
+    }
+
+    public void run() {
+        System.out.println("♕ Welcome to Chess. Sign in to start. ♕");
+        System.out.print(client.help());
+
+        Scanner scanner = new Scanner(System.in);
+        var result = "";
+        while (!result.equals("quit")) {
+            printPrompt();
+            String line = scanner.nextLine();
+
+            try {
+                result = client.eval(line);
+                System.out.print(result);
+            } catch (Throwable e) {
+                var msg = e.toString();
+                System.out.print(msg);
+            }
+        }
+        System.out.println();
+    }
+
+    private void printPrompt() {
+        System.out.print("\n" + "Chess>>> ");
+    }
+
 }
